b6043f4d51f9eb7377b8ad7fb3327138517c4133
Patrick Michaelsen <pmichaelsen@revolutionparts.com>
2016-10-11 10:34:29 -0700

    Update git_diff.sh

--- git_diff.sh
+++ git_diff.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# outputs names of all files
+# with delta between current branch
+# compared to origin/master
+# across any git repos
+# found in any current subdir
+
 for d in */ ; do
   echo "$d"
   git -C "$d" diff --name-status origin/master
